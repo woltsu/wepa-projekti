7e83ec0bb8e16a4bd107a9448fa17a2b543a203b
Olli <oltsuwh@gmail.com>
2017-08-01 00:45:45 +0300

    test

--- src/main/java/wepa/config/MainConfig.java
+++ src/main/java/wepa/config/MainConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("production")
+@Profile("productin")
 public class MainConfig {
 
     @Bean
