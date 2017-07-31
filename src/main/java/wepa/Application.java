ccb59d897c7108cabad85e2a924f9004bf398be8
Olli <oltsuwh@gmail.com>
2017-08-01 00:43:35 +0300

    trying to get to work

--- src/main/java/wepa/Application.java
+++ src/main/java/wepa/Application.java
@@ -2,10 +2,14 @@ package wepa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@EnableAsync
+@EnableScheduling
 @SpringBootApplication
 public class Application {
     
