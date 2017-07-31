d90f5bf8dff6ba1f3c92087292602c5dd0a655a9
Olli <oltsuwh@gmail.com>
2017-08-01 01:08:01 +0300

    trying thymeleaf

--- src/main/java/wepa/controller/DefaultController.java
+++ src/main/java/wepa/controller/DefaultController.java
@@ -1,6 +1,9 @@
 package wepa.controller;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,8 +12,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("*")
 public class DefaultController {
     
+//    @Autowired
+//    private BasicDataSource dataSource;
+//    
     @RequestMapping(method = RequestMethod.GET)
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("name", "olli");
         return "index";
     }
     
