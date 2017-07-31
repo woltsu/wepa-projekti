28cd6bb4f75458ca9fc42227ff5f6871cc6152d6
Olli <oltsuwh@gmail.com>
2017-07-31 23:36:15 +0300

    configuration

---
+++ src/main/java/wepa/configuration/DatabaseConfig.java
@@ -0,0 +1,19 @@
+package wepa.configuration;
+
+import javax.activation.DataSource;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class DatabaseConfig {
+    @Bean
+    @Primary
+ 
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return (DataSource) DataSourceBuilder.create().build();
+    }
+}
