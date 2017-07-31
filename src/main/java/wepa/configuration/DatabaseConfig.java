da7ebeaaa1c577e75ad06957daa9b1db43c0d25d
Olli <oltsuwh@gmail.com>
2017-07-31 23:38:39 +0300

    fix

--- src/main/java/wepa/configuration/DatabaseConfig.java
+++ src/main/java/wepa/configuration/DatabaseConfig.java
@@ -6,12 +6,14 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("production")
 public class DatabaseConfig {
+    
     @Bean
     @Primary
- 
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource dataSource() {
         return (DataSource) DataSourceBuilder.create().build();
