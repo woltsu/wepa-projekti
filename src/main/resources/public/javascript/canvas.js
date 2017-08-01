22a95c85e2ffb8ae5cf7235af3193bec1d4a128b
Olli <oltsuwh@gmail.com>
2017-08-01 17:50:41 +0300

    testing

--- src/main/resources/public/javascript/canvas.js
+++ src/main/resources/public/javascript/canvas.js
@@ -1,6 +1,3 @@
 $(document).ready(function () {
-    var canvas = document.getElementById("myCanvas");
-    var ctx = canvas.getContext("2d");
-    ctx.fillStyle = "#FF0000";
-    ctx.fillRect(0, 0, 150, 75);
+    alert("toimii");
 });
