bc80558d19438bfbb8fbdf3e880a38b72dad149f
Olli <oltsuwh@gmail.com>
2017-08-01 17:47:16 +0300

    jquery

--- src/main/resources/public/javascript/canvas.js
+++ src/main/resources/public/javascript/canvas.js
@@ -1,4 +1,6 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(0,0,150,75);
+$(document).ready(function () {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(0, 0, 150, 75);
+});
