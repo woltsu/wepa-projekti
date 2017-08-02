c5429ed558a90d0690a3f84b5c239d37cc48b32f
Olli <oltsuwh@gmail.com>
2017-08-02 16:11:17 +0300

    trying to get authentication to work

--- src/main/java/wepa/config/SecurityConfiguration.java
+++ src/main/java/wepa/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package wepa.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,7 +16,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/login").permitAll().anyRequest().authenticated();
+        http.authorizeRequests().antMatchers("/login", "/signup",
+                "/css/**", "/fonts/**", "/images/**", "/js/**", "/media/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/persons").permitAll()
+                .antMatchers(HttpMethod.POST, "/signup").permitAll()
+                .anyRequest().authenticated();
 
         http.formLogin()
                 .loginPage("/login")
@@ -30,6 +35,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/login")
                 .permitAll()
                 .invalidateHttpSession(true);
+    }
     }
 
     @Configuration
