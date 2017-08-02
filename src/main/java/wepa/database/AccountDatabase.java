a61987b85f86a2bfbf160370bd2b529d18fa4459
Olli <oltsuwh@gmail.com>
2017-08-02 19:31:33 +0300

    trying to get authentication to work

--- src/main/java/wepa/database/AccountDatabase.java
+++ src/main/java/wepa/database/AccountDatabase.java
@@ -23,7 +23,7 @@ public class AccountDatabase {
     public void init() {
         try (Connection conn = dataSource.getConnection()) {
             Statement st = conn.createStatement();
-            st.executeUpdate("CREATE TABLE Account (username varchar(25), password varchar(25));");
+            st.executeUpdate("CREATE TABLE Account (username varchar(25), password varchar(200));");
             create("user", "user");
             create("HELLO", "passu");
         } catch (Exception e) {
