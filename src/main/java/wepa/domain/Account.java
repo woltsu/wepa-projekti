a855821173e73c5bf1a856f3d29bf7a88b1e72ef
Olli <oltsuwh@gmail.com>
2017-08-02 19:22:11 +0300

    trying to get authentication to work

--- src/main/java/wepa/domain/Account.java
+++ src/main/java/wepa/domain/Account.java
@@ -26,8 +26,8 @@ public class Account {
     }
 
     public void setPassword(String password) {
-        this.salt = BCrypt.gensalt();
-        this.password = BCrypt.hashpw(password, this.salt);
+//        this.salt = BCrypt.gensalt();
+        this.password = password;
     }
 
     public void setSalt(String salt) {
