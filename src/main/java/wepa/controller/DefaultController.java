5cbe958ad41d7dfca6fb8c9fd44f7d3dbb325a2e
Olli <oltsuwh@gmail.com>
2017-08-03 17:18:35 +0300

    getting authenticated user

--- src/main/java/wepa/controller/DefaultController.java
+++ src/main/java/wepa/controller/DefaultController.java
@@ -6,32 +6,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import wepa.database.AccountDatabase;
+import wepa.service.AccountService;
 
 @Controller
 @RequestMapping("*")
 public class DefaultController {
-    
+
     @Autowired
     private AccountDatabase accountDatabase;
     
+    @Autowired
+    private AccountService accountService;
+
     @RequestMapping(method = RequestMethod.GET)
     public String home(Model model) {
+        model.addAttribute("user", accountService.getAuthenticatedAccount());
         return "index";
     }
-    
+
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String login(Model model) {
-        model.addAttribute("user", accountDatabase.findByUsername("HELLO").getUsername());
-        model.addAttribute("users", accountDatabase.getUsers());
+    public String login() {
         return "login";
     }
-    
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String test() {
-        accountDatabase.create("TOIMII", "JES!!");
-        return "redirect:/login";
-    }
-    
-  
-    
+
 }
