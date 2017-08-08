042214d0b944be808b92b17324f7ac07297acdf9
Olli <oltsuwh@gmail.com>
2017-08-08 11:11:02 +0300

    fixing bugs

--- src/main/java/wepa/local/repository/LocalTestRepository.java
+++ src/main/java/wepa/local/repository/LocalTestRepository.java
@@ -1,10 +1,12 @@
 package wepa.local.repository;
 
 import java.util.List;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import wepa.local.domain.LocalAccount;
 import wepa.local.domain.LocalTest;
 
+@Profile("default")
 public interface LocalTestRepository extends JpaRepository<LocalTest, Long> {
 
     List<LocalTest> findByLocalAccount(LocalAccount a);
