c0e635a04c4ed5e110c721142c63420c89315598
Olli <oltsuwh@gmail.com>
2017-08-13 23:44:56 +0300

    trying to get remember me to work. Added customuserdetailsservice

--- src/main/java/wepa/config/SecurityConfiguration.java
+++ src/main/java/wepa/config/SecurityConfiguration.java
@@ -1,14 +1,18 @@
 package wepa.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.encoding.PasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import wepa.auth.CustomAuthenticationProvider;
 
 @Profile("production")
@@ -16,9 +20,12 @@ import wepa.auth.CustomAuthenticationProvider;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        
+
         http.csrf().disable().authorizeRequests().antMatchers("/login", "/signup",
                 "/css/**", "/fonts/**", "/images/**", "/js/**", "/media/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/signup").permitAll()
@@ -37,6 +44,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/login")
                 .permitAll()
                 .invalidateHttpSession(true);
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Profile("production")
---
+++ src/main/java/wepa/service/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package wepa.service;
+
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import wepa.database.AccountDatabase;
+import wepa.domain.Account;
+
+@Profile("production")
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private AccountDatabase accountDatabase;
+    
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Account account = accountDatabase.findByUsername(username);
+        if (account == null) {
+            throw new UsernameNotFoundException("No such user: " + username);
+        }
+
+        return new org.springframework.security.core.userdetails.User(
+                account.getUsername(),
+                account.getPassword(),
+                true,
+                true,
+                true,
+                true,
+                Arrays.asList(new SimpleGrantedAuthority("USER")));
+    }
+}
