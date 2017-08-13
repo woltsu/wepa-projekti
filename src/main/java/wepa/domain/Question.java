0258876b44b46df7eba6dc4dd2fa16462fc8755b
Olli <oltsuwh@gmail.com>
2017-08-13 18:47:16 +0300

    trying to fix problems with the index page in heroku (question.publisher.username throws a thymeleaf error)

--- src/main/java/wepa/domain/Question.java
+++ src/main/java/wepa/domain/Question.java
@@ -60,8 +60,8 @@ public class Question {
         this.published = published;
     }
     
-    public int getPublisher() {
-        return account_id;
+    public Account getPublisher() {
+        return accountDatabase.findOne(account_id);
     }
     
 }
