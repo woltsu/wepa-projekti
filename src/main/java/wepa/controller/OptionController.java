6f6aec200bf2cf31d8c70c079bfa67a030a867d7
Olli <oltsuwh@gmail.com>
2017-08-16 21:33:39 +0300

    Added option validator for heroku

--- src/main/java/wepa/controller/OptionController.java
+++ src/main/java/wepa/controller/OptionController.java
@@ -1,5 +1,6 @@
 package wepa.controller;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
@@ -10,8 +11,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import wepa.database.OptionDatabase;
 import wepa.database.QuestionDatabase;
+import wepa.domain.Account;
 import wepa.domain.Option;
+import wepa.domain.Question;
+import wepa.service.AccountService;
 import wepa.service.OptionService;
+import wepa.validator.OptionValidator;
 
 @Profile("production")
 @Controller
@@ -26,11 +31,24 @@ public class OptionController {
     @Autowired
     private OptionDatabase optionDatabase;
 
+    @Autowired
+    private OptionValidator optionValidator;
+    
+    @Autowired
+    private AccountService accountService;
+
     @RequestMapping(value = "/{user}/questions/{id}", method = RequestMethod.POST)
     public String postOption(Model model, @PathVariable int id, Option option, @PathVariable String user) {
-        if ((optionService.hasAlreadyACorrectOption(id) && option.isCorrect()) || (optionService.hasMaxFalseOptions(id) && !option.isCorrect())) {
-            return "redirect:/" + user + "/questions/" + id;
+        Account self = accountService.getAuthenticatedAccount();
+        List<String> errors = optionValidator.validateOption(option);
+        if (!errors.isEmpty()) {
+            model.addAttribute("errors", errors);
+            model.addAttribute("question", questionDatabase.findOne(id));
+            model.addAttribute("user", self);
+            model.addAttribute("options", optionDatabase.findByQuestion(id));
+            return "question";
         }
+
         option.setQuestion_id(questionDatabase.findOne(id).getId());
         optionDatabase.create(option.getValue(), option.isCorrect(), option.getQuestion_id());
         return "redirect:/" + user + "/questions/" + id;
@@ -39,6 +57,9 @@ public class OptionController {
     @RequestMapping(value = "/{user}/questions/{id}", method = RequestMethod.DELETE)
     public String deleteOption(Model model, @PathVariable int id, @RequestParam int option_id, @PathVariable String user) {
         optionDatabase.delete(option_id);
+        Question q = questionDatabase.findOne(id);
+        q.setPublished(false);
+        questionDatabase.save(q);
         return "redirect:/" + user + "/questions/" + id;
     }
 
---
+++ src/main/java/wepa/validator/OptionValidator.java
@@ -0,0 +1,38 @@
+package wepa.validator;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import wepa.domain.Option;
+import wepa.service.OptionService;
+
+@Profile("production")
+@Component
+public class OptionValidator {
+
+    @Autowired
+    private OptionService optionService;
+
+    public List<String> validateOption(Option o) {
+        List<String> errors = new ArrayList();
+        errors.addAll(validateValue(o.getValue()));
+        if (optionService.hasAlreadyACorrectOption(o.getQuestion_id()) && o.isCorrect()) {
+            errors.add("Question already has a correct option!");
+        }
+        if (optionService.hasMaxFalseOptions(o.getQuestion_id()) && !o.isCorrect()) {
+            errors.add("Question has maxium number of incorrect options!");
+        }
+        return errors;
+    }
+
+    private List<String> validateValue(String value) {
+        List<String> errors = new ArrayList();
+        if (value == null || value.isEmpty()) {
+            errors.add("Option value must not be empty!");
+        }
+        return errors;
+    }
+
+}
