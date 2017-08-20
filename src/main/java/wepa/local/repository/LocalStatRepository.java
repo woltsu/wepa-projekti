a6a43f27f969da578261ebe58d59dfe39cdb3eab
Olli <oltsuwh@gmail.com>
2017-08-20 18:16:39 +0300

    Added stats for users

--- src/main/java/wepa/local/repository/LocalStatRepository.java
+++ src/main/java/wepa/local/repository/LocalStatRepository.java
@@ -1,9 +1,11 @@
 package wepa.local.repository;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import wepa.local.domain.LocalAccount;
 import wepa.local.domain.LocalStat;
 
+@Profile("default")
 public interface LocalStatRepository extends JpaRepository<LocalStat, Long> {
 
     LocalStat findByAccount(LocalAccount a);
