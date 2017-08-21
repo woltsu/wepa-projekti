9bcac93510eb38f8f23f8ccc23f0f64944f1656e
Olli <oltsuwh@gmail.com>
2017-08-21 20:52:17 +0300

    now user can only input numbers into the search input

--- src/main/resources/public/javascript/validate.js
+++ src/main/resources/public/javascript/validate.js
@@ -2,4 +2,4 @@ function forceNumeric(){
     var $input = $(this);
     $input.val($input.val().replace(/[^\d]+/g,''));
 }
-$('search').on('propertychange input', 'input[type="number"]', forceNumeric);
+$('body').on('propertychange input', 'input[type="number"]', forceNumeric);
